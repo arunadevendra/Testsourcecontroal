87154383e24e0c5473b92d1f95d15f789d87285f
arunadevendra <83600474+arunadevendra@users.noreply.github.com>
2022-06-07 17:41:34 +0530

    Delete Delet the Automation Account.ps1

--- Delet the Automation Account.ps1
+++
@@ -1,7 +0,0 @@
-﻿#Connect-AzAccount
-
-Add-AzureAccount
-#Select-AzSubscription -SubscriptionId 'a3463f52-da99-49bd-86ac-3ccd110e834d'
-
-
-Remove-AzAutomationAccount -Name "testgenevaactioncbn" -Force -ResourceGroupName "Testautomationcbn" 
